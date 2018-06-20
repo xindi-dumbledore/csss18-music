09fd99861991b1c2bcc2680bf67abd83f459416d
Ricky <rlaishra@syr.edu>
2018-06-19 19:28:06 -0600

    Data formatted for HON

--- Data/convert_hondata.py
+++ Data/convert_hondata.py
@@ -1,34 +1,37 @@
 import sys
 import json
 import csv
+import os
 
-def readData(fname):
+def readData(fname, sname):
 	with open(fname, 'r') as f:
 		jdata = json.load(f)
 	
-	data = []
+	#data = {}
 	counter = 1
 	for name in jdata:
+		#data[name] = {}
 		for i in jdata[name][1]:
 			if len(jdata[name][1][i]) > 0:
 				row = jdata[name][1][i]
 				row = [r if isinstance(r, int) else max(r) for r in row]
-				data.append(row)
-				if counter == 724:
-					print(name, i)
-				counter += 1
-	return data
+				#data[name][i] = row
+				if len(row) > 0:
+					print('Saving : {} {}'.format(name, str(i)))
+					print(os.path.join(sname, name[1:].replace('/', ' ') + '_' + i + '.csv'))
+					saveData(os.path.join(sname, name[1:].replace('/', ' ') + '_' + i + '.csv'), row)
+	
+	#return data
 
 def saveData(sname, data):
 	with open(sname, 'w') as f:
 		writer = csv.writer(f, delimiter=' ')
-		for row in data:
-			writer.writerow(row)
+		writer.writerow(data)
 
 
 if __name__ == '__main__':
 	fname = sys.argv[1]
 	sname = sys.argv[2]
 
-	data = readData(fname)
+	readData(fname, sname)
 	#saveData(sname, data)
