dfba9d556bc5017397cb09fc532e457f1faeb64b
Ricky <rlaishra@syr.edu>
2018-06-20 22:58:15 -0600

    Removed 128 from trajectory (for testing)

--- Data/convert_hondata.py
+++ Data/convert_hondata.py
@@ -16,17 +16,37 @@ def readData(fname, sname):
 				row = jdata[name][1][i]
 				row = [r if isinstance(r, int) else max(r) for r in row]
 				#data[name][i] = row
-				if len(row) > 0:
+				rows = thresholding(row)
+				if len(rows) > 0:
 					print('Saving : {} {}'.format(name, str(i)))
 					print(os.path.join(sname, name[1:].replace('/', ' ') + '_' + i + '.csv'))
-					saveData(os.path.join(sname, name[1:].replace('/', ' ') + '_' + i + '.csv'), row)
+					saveData(os.path.join(sname, name[1:].replace('/', ' ') + '_' + i + '.csv'), rows)
 	
 	#return data
+
+
+def thresholding(chords):
+	"""
+	Remove short rests, keep longer ones, and split into differet lines on very long rest
+	"""
+	#lower = 1
+	#upper = 3
+
+	chord_str = ' '.join(map(str, chords))
+	chords = chord_str.split('128 128 128')
+	chords = [c.replace('128 128', '128') for c in chords]
+	chords = [c.replace('128', '') for c in chords]
+	chords = [c.split(' ') for c in chords]
+	chords = [[int(i) for i in c if len(i) > 0] for c in chords]
+	#chord_str = chord_str.replace('128', '')
+	return chords
+
 
 def saveData(sname, data):
 	with open(sname, 'w') as f:
 		writer = csv.writer(f, delimiter=' ')
-		writer.writerow(data)
+		for row in data:
+			writer.writerow(row)
 
 
 if __name__ == '__main__':
