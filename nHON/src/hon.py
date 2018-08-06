512921dd9a6c4f6dff7bc78d6264bc2d1a1ac4f3
Ricky <rlaishra@syr.edu>
2018-08-06 01:25:32 -0400

    new HON with time intervals

---
+++ nHON/src/hon.py
@@ -0,0 +1,156 @@
+"""
+Creates HON from trajectories
+One line in the input file is considered one trajectory
+"""
+
+import sys
+import csv
+
+class HON(object):
+	"""docstring for HON"""
+	def __init__(self):
+		super(HON, self).__init__()
+	
+
+	def get_edges(self, fname, delta_confidence=0.05, min_support=10, sep='\t', max_prior=-1):
+		"""
+		Reads the input trajectory file and construct the HON
+		Inputs:
+			fname : The file name of the trajectory
+			confidence: The minimum confidence required to include a rule (path)
+			support: The minimum number of times the rule (path) is observed to consider it for HON
+			seperator: The node separator in trajectory file
+			max_prior: The maximum order to consider (-1 is unlimited) (Increasing this number slows down algorithm.)
+		"""
+		paths = {}
+		
+		with open(fname, 'r') as file:
+			lnum = 0
+			for line in file:
+				lnum += 1
+				line = line.rstrip('\n')
+				nodes = line.split(sep)
+				if len(nodes) < 2:
+					continue
+
+				for i in range(0, len(nodes)-1):
+					target = nodes[i + 1]
+					
+					if max_prior == -1:
+						k = 0
+					else:
+						k = max(0, i - max_prior)
+
+					for j in range(k, i):
+						source = tuple(nodes[j:i])
+						#print(source, target)
+
+						if source not in paths:
+							paths[source] = {}
+
+						if target not in paths[source]:
+							paths[source][target] = {'confidence':0, 'support':0}
+
+						paths[source][target]['support'] += 1
+				print('Line # {}'.format(lnum))
+
+		# Remove paths with less than min_support and min_confidence
+		for source in paths:
+			remove = []
+			path_sum = 0
+
+			for target in paths[source]:
+				if paths[source][target]['support'] < min_support:
+					remove.append(target)
+
+				path_sum += paths[source][target]['support']
+
+			for target in paths[source]:
+				paths[source][target]['confidence'] = paths[source][target]['support']/path_sum
+
+			if len(source) > 1:
+				# Remove only higher order rules
+				for target in remove:
+					del paths[source][target]
+
+		# Remove paths with confidence with low confidence delta
+		# If the confidence does not imporve by at least delta_confidence by adding one more order, dont add the higher order
+		for source in paths:
+			if len(source) == 1:
+				continue
+
+			# Confidence from lower orders
+
+			remove = []
+			p_source = source[1:]
+
+			if p_source not in paths:
+				continue
+
+			for target in paths[source]:
+				p_confidence = max([paths[source[i:]][target]['confidence'] for i in range(1, len(source)) if source[i:] in paths and target in paths[source[i:]]])
+				#if target in paths[p_source] and (paths[source][target]['confidence'] - paths[p_source][target]['confidence'] < delta_confidence):
+				if abs(paths[source][target]['confidence'] - p_confidence) < delta_confidence:
+					remove.append(target)
+
+			for target in remove:
+				del paths[source][target]
+
+
+		# Remove paths with no targets
+		remove = []
+		for source in paths:
+			if len(paths[source]) == 0:
+				remove.append(source)
+
+		for source in remove:
+			del paths[source]
+
+
+		# Adjust support and confidence of paths
+		for source in paths:
+			for target in paths[source]:
+				# Supports from higher rules
+				h_support = sum([paths[s][target]['support'] for s in paths if len(s) > len(source) and s[-len(source):] == source and target in paths[s]])
+				paths[source][target]['confidence'] = paths[source][target]['confidence'] * (paths[source][target]['support'] - h_support)/paths[source][target]['support']
+				paths[source][target]['support'] -= h_support
+
+		# Reformat the edges
+		edges = {}
+		for source in paths:
+			for target in paths[source]:
+				s = source[-1]
+				t = target
+				p = source
+				
+				if (s, t) not in edges:
+					edges[(s,t)] = []
+
+				edges[(s,t)].append({'path':p, 'confidence': paths[source][target]['confidence'], 'support': paths[source][target]['support']})
+		
+		return edges
+
+
+	def save_edges(self, edges, sname, delim='\t'):
+		"""
+		Write the HON edgelist to a file
+		"""
+		with open(sname, 'w') as f:
+			writer = csv.writer(f, delimiter=delim)
+			writer.writerow(['Source', 'Target', 'Path', 'Support', 'Confidence'])
+
+			for e in edges:
+				for d in edges[e]:
+					writer.writerow([e[0], e[1], '>'.join(d['path']), d['support'], d['confidence']])
+
+	## Todo generate graph, pagerank, degree, centralities etc.
+
+		
+
+if __name__ == '__main__':
+	fname = sys.argv[1]
+	sname = sys.argv[2]
+
+	h = HON()
+	edges = h.get_edges(fname, sep='\t', max_prior=10, min_support=10, delta_confidence=0.05)
+	h.save_edges(edges, sname)
---
+++ nHON/src/midi_to_trajectory.py
@@ -0,0 +1,116 @@
+import sys
+import json
+import csv
+import os
+
+pitch_map = {
+	'C':	60,
+	'C#':	61,
+	'D':	62,
+	'D#':	63,
+	'E':	64,
+	'F': 	65,
+	'F#':	66,
+	'G':	67,
+	'G#':	68,
+	'A': 	69,
+	'A#':	70,
+	'B':	71,
+	'D-':	61,
+	'E-':	63,
+	'F-':	64,
+	'G-':	66,
+	'A-':	68,
+	'B-':	70 	
+}
+
+def readData(fname):
+	with open(fname, 'r') as f:
+		jdata = json.load(f)
+
+	trajectories = [] 				# all the trajectories
+
+	for name in jdata:
+		# Get pitch of song to convert to relative pitch
+		pitch = jdata[name][0].split(' ')[0]
+		pitch = pitch_map[pitch]
+
+		mrow = [] 		# notes
+		trow = []		# time intervals
+		m_index = -1	# the index of mrow
+
+		for i in jdata[name][1]:
+			if len(jdata[name][1][i]) == 0:
+				continue
+
+			row = jdata[name][1][i]
+			row = [int(r) if isinstance(r, int) else max(r) for r in row]			# In case of multiple notes at same position, select max
+			#row = [r for r in row if r != 128] 									# Remove pause
+			row = [r - pitch if r < 128 else r for r in row] 						# Convert to relative pitch (only non pauses)
+			
+			if len(row) > len(mrow):
+				mrow = row
+				m_index = i
+
+		if m_index == -1:
+			continue
+
+		#t_max = jdata[name][2][m_index][-1][1]										# Length of the piece (used for normalization)
+		trow = [(v[1]-v[0]) for v in jdata[name][2][m_index]]
+		#print(trow)
+
+		# Merge consecutive identical notes
+		# Note: Optimize this 
+		l_max = len(mrow)
+		l = l_max
+		while l > 1:
+			if l > len(mrow):
+				continue
+
+			dups = []
+			for i in range(0, len(mrow)-l+1):
+				if len(set(mrow[i:i+l])) == 1:
+					dups = list(range(i,i+l))
+
+					trow[min(dups) + 1] = sum(trow[min(dups):max(dups) + 1]) 			# sum all the time for nodes about to be removed
+					for r in sorted(dups, reverse=True):
+						if r == max(dups):
+							continue
+						mrow.pop(r)
+						trow.pop(r)
+
+					break
+			
+			if len(dups) == 0:
+				l -= 1
+
+		# round the time
+		trow = [round(v,2) for v in trow]
+
+		# Filter out notes wih 0 interval
+		traj = []
+		for i in range(0, len(mrow)):
+			if trow[i] > 0:
+				traj.append('{}_{}'.format(mrow[i], trow[i]))
+
+		trajectories.append(traj)
+
+		print('Processed: {}'.format(name))
+
+	return trajectories
+
+
+def saveTrajectory(data, sname):
+	with open(sname, 'w') as f:
+		writer = csv.writer(f, delimiter='\t')
+		for row in data:
+			writer.writerow(row)
+
+
+if __name__ == '__main__':
+	fname = sys.argv[1]
+	sname = sys.argv[2]
+
+	tdata = readData(fname)
+	saveTrajectory(tdata, sname)
+		
